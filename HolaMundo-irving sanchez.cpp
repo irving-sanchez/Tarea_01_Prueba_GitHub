309a869c43b0cae6bad9184b1950a323e2dfdf95
irving-sanchez <79544943+irving-sanchez@users.noreply.github.com>
2021-02-23 16:20:21 -0600

    Agregando archivo

---
+++ HolaMundo-irving sanchez.cpp
@@ -0,0 +1,13 @@
+//la biblioteca principal
+#include<iostream>
+
+using namespace std;
+
+// la funcion principal
+int main(){
+       
+	   cout<<"\n Hola mundo Me llamo Irving Yahir Sanchez "<<endl;	
+	
+	//La salida del main
+	return 1;
+}
